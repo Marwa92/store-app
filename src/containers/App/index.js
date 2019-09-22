590e4aaedd25f5de730311b9e9134eb44b929c56
Marwa Yahia Galal <engmarwayehya@yahoo.com>
2019-09-22 23:02:26 +0200

    Create new app.

---
+++ src/containers/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+
+const App = () => (
+    <Router>
+    <div>
+      test
+    </div>
+  </Router>
+)
+
+export default App;
