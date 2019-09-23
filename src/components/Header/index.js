dc03ca631b706d89e3a5453f2f08b10d6e9bde3c
Marwa Yahia Galal <engmarwayehya@yahoo.com>
2019-09-24 00:25:49 +0200

    add Header to app.

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Text } from 'grommet'
+
+const Header = () =>(
+<Box fill alignContent="center" justify="center" background={{color: 'brand'}}>
+<Text alignSelf="center" size="large">StoreApp</Text>
+</Box>
+)
+
+
+export default Header;
